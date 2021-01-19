a8d83e91569270896d6c462e6c187110d7dbe4ea
Aashiq <ashik@parallaxlogic.com>
2021-01-19 14:18:35 +0600

    byte array fixed

--- app/src/main/java/com/parallaxlogic/testjava/MainActivity.java
+++ app/src/main/java/com/parallaxlogic/testjava/MainActivity.java
@@ -1,12 +1,9 @@
 package com.parallaxlogic.testjava;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,15 +13,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
-
 
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.WHITE;
@@ -32,8 +26,8 @@ import static android.graphics.Color.WHITE;
 public class MainActivity extends AppCompatActivity {
 
     private Bitmap bitmap;
-Button btn,btn2,btn3;
-ImageView iv,iv2;
+    Button btn;
+    ImageView iv;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -42,15 +36,11 @@ ImageView iv,iv2;
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btn);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
         iv = findViewById(R.id.iv);
-        iv2 = findViewById(R.id.iv2);
+
 
 
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.i100);
-        // you can change the format of you image compressed for what do you want;
-        // now it is set up to 640 x 480;
         Bitmap bmpCompressed = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
@@ -77,20 +67,9 @@ ImageView iv,iv2;
 
 
 
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
-            }
-        });
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
-
 
                 QRCodeWriter writer = new QRCodeWriter();
                 try {
@@ -115,17 +94,6 @@ ImageView iv,iv2;
         });
 
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                iv2.setImageBitmap(bitmap);
-                Toast.makeText(getApplicationContext(), "Decoded", Toast.LENGTH_SHORT).show();
-
-
-            }
-        });
 
 
     }
